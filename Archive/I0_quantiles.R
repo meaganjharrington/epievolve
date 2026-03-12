8bdab25da42b91714ee925628e2b343956fdfab3
meaganjharrington <meaganjharrington@gmail.com>
2026-03-12 12:26:52 +0000

    cleaning cont.

--- Archive/I0_quantiles.R
+++
@@ -1,5 +0,0 @@
-#' Build deterministic I0 posterior summary object
-#' @keywords internal
-I0_quantiles <- function(I0_samp, q3) {
-  q3(I0_samp)  # return the named numeric vector: c("2.5%","50%","97.5%")
-}
--- Archive/likelihood_build_generator.R
+++
@@ -1,6 +0,0 @@
-#' Create odin2 generator for SIR with time-varying beta
-#' @keywords internal
-likelihood_build_generator <- function() {
-  # Reuses your existing generator factory (which may be cached internally)
-  make_sir_timevary_generator()
-}
